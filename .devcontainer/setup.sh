b0a2f2e4a19c027aa3c502bd941bbabf1abb8f60
Alan Ruiz <alanzphy@gmail.com>
2026-03-18 21:48:49 -0600

    fix devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -10,5 +10,5 @@ mkdir -p ./system/legacy/backups/2026/hidden_config/auth/
 # 3. La Magia: Usamos la variable de entorno para crear el archivo localmente
 # Si el secreto existe, lo guardamos en el archivo escondido.
 if [ ! -z "$DISCORD_WEBHOOK" ]; then
-    echo "$DISCORD_WEBHOOK" > ./system/legacy/backups/2026/hidden_config/auth/auth/credenciales_discord.txt
+    echo "$DISCORD_WEBHOOK" > ./system/legacy/backups/2026/hidden_config/auth/credenciales_discord.txt
 fi
