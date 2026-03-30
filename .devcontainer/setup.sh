0ad310c0d4da97604307f3f3d78cffe378c7339c
Alan Ruiz <alanzphy@gmail.com>
2026-03-29 18:10:20 -0600

    fix devcontainer.json

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,12 +3,8 @@
 sudo apt-get update
 sudo apt-get install -y figlet
 
-# 2. Creamos el laberinto para la Búsqueda del Tesoro
-# (Se creará una estructura de carpetas confusa dentro de su espacio de trabajo)
 mkdir -p ./system/legacy/backups/2026/hidden_config/auth/
 
-# 3. La Magia: Usamos la variable de entorno para crear el archivo localmente
-# Si el secreto existe, lo guardamos en el archivo escondido.
 if [ ! -z "$DISCORD_WEBHOOK" ]; then
     echo "$DISCORD_WEBHOOK" > ./system/legacy/backups/2026/hidden_config/auth/credenciales_discord.txt
 fi
